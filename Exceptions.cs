e92f2e3d9448fda28a59ca18361baef82207f76a
@Jeremyobrien <jjob1391@gmail.com>
2022-07-14 16:17:22 -0400

    Push to repo

---
+++ Exceptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace TowerDefense
+{
+    public class TreehouseDefenseException : System.Exception
+    {
+        public TreehouseDefenseException()
+        {
+        }
+        public TreehouseDefenseException(string message) : base(message)
+        {
+        }
+    }
+    public class OutOfBoundsException : TreehouseDefenseException
+    {
+        public OutOfBoundsException()
+        {
+        }
+        public OutOfBoundsException(string message) : base(message)
+        {
+        }
+
+    }
+
+}
+
